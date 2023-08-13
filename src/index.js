644b6c0190beed487a03be17d057d11a7399fbc8
HanKorea <nguyennamhoai01042002@gmail.com>
2023-08-13 22:36:18 +0700

    chu-nhat

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,27 +26,11 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-console.log('PATH :',path.join(__dirname, 'resources\\views'));
+// console.log('PATH :',path.join(__dirname, 'resources\\views'));
 
+//Route
+route(app);
 
-app.get('/', (req, res) => { 
-   res.render('home');
-})
-
-app.get('/news', (req, res) => { 
-  res.render('news');
-})
-
-app.get('/search', (req, res) => { 
-  res.render('search');
-})
-
-app.post('/search', (req, res) => { 
-
-  console.log(req.body);
-
-  res.send('');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
